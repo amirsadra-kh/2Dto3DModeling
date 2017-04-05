d523083d549b971d3c5affbbe546dae9440c3a02
Amir Sadra Khorramizadeh <amirsadra@me.com>
2017-04-05 18:07:57 -0400

    Initial Commit

---
+++ FinalProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FinalProject
+//
+//  Created by Amir Sadra on 4/5/17.
+//  Copyright © 2017 Amir Sadra. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
